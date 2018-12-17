159b30d21b3d33acfac068f60a95f0dbf93e30fd
Drew Atkinson <drewatkinson5@gmail.com>
2018-12-17 13:49:54 -0500

    Start work on tests, allow using TS for dev mode main process

--- app/menu.ts
+++ app/menu.ts
@@ -1,13 +1,7 @@
-import {
-  app,
-  BrowserWindow,
-  Menu,
-  MenuItemConstructorOptions,
-  shell
-} from 'electron';
+import { app, BrowserWindow, Menu, shell } from 'electron';
 
 export default class MenuBuilder {
-  private mainWindow: BrowserWindow;
+  public mainWindow: BrowserWindow;
 
   constructor(mainWindow: BrowserWindow) {
     this.mainWindow = mainWindow;
@@ -21,13 +15,10 @@ export default class MenuBuilder {
       this.setupDevelopmentEnvironment();
     }
 
-    // TODO: Enable mac template
-    // const template =
-    //   process.platform === 'darwin'
-    //     ? this.buildDarwinTemplate()
-    //     : this.buildDefaultTemplate();
-
-    const template = this.buildDefaultTemplate();
+    const template =
+      process.platform === 'darwin'
+        ? this.buildDarwinTemplate()
+        : this.buildDefaultTemplate();
 
     const menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
@@ -36,7 +27,7 @@ export default class MenuBuilder {
   }
 
   public setupDevelopmentEnvironment() {
-    this.mainWindow.webContents.openDevTools();
+    this.mainWindow.openDevTools();
     this.mainWindow.webContents.on('context-menu', (e, props) => {
       const { x, y } = props;
 
@@ -44,16 +35,148 @@ export default class MenuBuilder {
         {
           label: 'Inspect element',
           click: () => {
-            this.mainWindow.webContents.inspectElement(x, y);
+            this.mainWindow.inspectElement(x, y);
           }
         }
-      ]).popup({ window: this.mainWindow });
+      ]).popup(this.mainWindow);
     });
   }
 
   public buildDarwinTemplate() {
-    // TODO: Rebuild Darwin Menu, see https://github.com/electron/electron/blob/master/docs/api/menu.md#notes-on-macos-application-menu
-    return [];
+    const subMenuAbout = {
+      label: 'Electron',
+      submenu: [
+        {
+          label: 'About ElectronReact',
+          selector: 'orderFrontStandardAboutPanel:'
+        },
+        { type: 'separator' },
+        { label: 'Services', submenu: [] },
+        { type: 'separator' },
+        {
+          label: 'Hide ElectronReact',
+          accelerator: 'Command+H',
+          selector: 'hide:'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          selector: 'hideOtherApplications:'
+        },
+        { label: 'Show All', selector: 'unhideAllApplications:' },
+        { type: 'separator' },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    };
+    const subMenuEdit = {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:'
+        }
+      ]
+    };
+    const subMenuViewDev = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'Command+R',
+          click: () => {
+            this.mainWindow.webContents.reload();
+          }
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          }
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: 'Alt+Command+I',
+          click: () => {
+            this.mainWindow.toggleDevTools();
+          }
+        }
+      ]
+    };
+    const subMenuViewProd = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Toggle Full Screen',
+          accelerator: 'Ctrl+Command+F',
+          click: () => {
+            this.mainWindow.setFullScreen(!this.mainWindow.isFullScreen());
+          }
+        }
+      ]
+    };
+    const subMenuWindow = {
+      label: 'Window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'Command+M',
+          selector: 'performMiniaturize:'
+        },
+        { label: 'Close', accelerator: 'Command+W', selector: 'performClose:' },
+        { type: 'separator' },
+        { label: 'Bring All to Front', selector: 'arrangeInFront:' }
+      ]
+    };
+    const subMenuHelp = {
+      label: 'Help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            shell.openExternal('http://electron.atom.io');
+          }
+        },
+        {
+          label: 'Documentation',
+          click() {
+            shell.openExternal(
+              'https://github.com/atom/electron/tree/master/docs#readme'
+            );
+          }
+        },
+        {
+          label: 'Community Discussions',
+          click() {
+            shell.openExternal('https://discuss.atom.io/c/electron');
+          }
+        },
+        {
+          label: 'Search Issues',
+          click() {
+            shell.openExternal('https://github.com/atom/electron/issues');
+          }
+        }
+      ]
+    };
+
+    const subMenuView =
+      process.env.NODE_ENV === 'development' ? subMenuViewDev : subMenuViewProd;
+
+    return [subMenuAbout, subMenuEdit, subMenuView, subMenuWindow, subMenuHelp];
   }
 
   public buildDefaultTemplate() {
@@ -99,7 +222,7 @@ export default class MenuBuilder {
                   label: 'Toggle &Developer Tools',
                   accelerator: 'Alt+Ctrl+I',
                   click: () => {
-                    this.mainWindow.webContents.toggleDevTools();
+                    this.mainWindow.toggleDevTools();
                   }
                 }
               ]
